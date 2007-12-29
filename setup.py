64492f0a062a4c6b349752c4ab54aa6f964d29a5
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-12-29 18:02:10 +0000

    bump version
    git-svn-id: https://code.astraw.com/motmot/trunk/pygarrayimage@353 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pygarrayimage',
-      version='0.0.1',
+      version='0.0.2',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       url='http://code.astraw.com/projects/motmot/wiki/pygarrayimage',
