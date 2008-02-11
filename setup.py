642bfb6e62d2629de20189fad0daab34a1406939
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2008-02-11 05:17:08 +0000

    include setuptools install_requires (patch from Kristin Branson)
    git-svn-id: https://code.astraw.com/motmot/trunk/pygarrayimage@476 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(name='pygarrayimage',
       version='0.0.2', # keep in sync with arrayimage.py
+      #install_requires=['pyglet>=1.0',], # pyglet egg is broken
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       url='http://code.astraw.com/projects/motmot/wiki/pygarrayimage',
