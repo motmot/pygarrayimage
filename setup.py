ae17a56fdee72bd8a85ba4e43cc47333cada2bff
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2008-02-11 05:35:18 +0000

    add long_description attributes to setup.py files
    git-svn-id: https://code.astraw.com/motmot/trunk/pygarrayimage@477 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup
 
 setup(name='pygarrayimage',
+      description="Allow numpy arrays as source of texture data for pyglet.",
+      long_description = \
+"""pygarrayimage allows display of Python objects supporting the array
+interface as OpenGL textures without a copy. The OpenGL texture
+handling is done using pyglet.
+
+In other words, this allows fast transfer of data from numpy arrays
+(or any other data source supporting the array interface) to the video
+card.
+""",
       version='0.0.2', # keep in sync with arrayimage.py
       #install_requires=['pyglet>=1.0',], # pyglet egg is broken
       author='Andrew Straw',
